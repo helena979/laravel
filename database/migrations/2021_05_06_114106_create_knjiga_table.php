008e6d1cafa49677b6920746c0ffce8101317388
helena979 <helena.dragass1a@gmail.com>
2021-05-06 14:24:17 +0200

    adds additional column

--- database/migrations/2021_05_06_114106_create_knjiga_table.php
+++ database/migrations/2021_05_06_114106_create_knjiga_table.php
@@ -16,6 +16,7 @@ class CreateKnjigaTable extends Migration
         Schema::create('knjiga', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('author');
             $table->timestamps();
         });
     }
